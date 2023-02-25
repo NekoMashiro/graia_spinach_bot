bfdafcc01c5f78cf19b32ab051e7e991dc774353
haroldyhli <haroldyhli@haroldyhlideMacBook-Pro.local>
2023-02-25 15:02:55 +0800

    动态重载模块能力预埋

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from asyncio import wait_for
+from multiprocessing.connection import wait
 import pkgutil
 
 from creart import create
@@ -8,6 +10,23 @@ from graia.ariadne.connection.config import (
 from graia.broadcast import Broadcast
 from graia.saya import Saya
 
+saya = create(Saya)
+
+def doReloadModules():
+    print("============ 重载所有模块 ============")
+    # 先删除所有模块
+    for channel in saya.channels:
+        print("卸载模块" + channel.title)
+    print("============ 重新安装全部 ============")
+    # 再重新引入所有模块
+    with saya.module_context():
+        for module_info in pkgutil.iter_modules(["modules"]):
+            if module_info.name.startswith("_"):
+                continue
+            saya.require(f"modules.{module_info.name}")
+            print("安装模块" + module_info.name)
+    print("============ 重载模块完成 ============")
+
 if __name__ == '__main__':
     # 机器人init
     app = Ariadne(
@@ -17,11 +36,6 @@ if __name__ == '__main__':
         ),
     )
     # 引入所有模块
-    saya = create(Saya)
-    with saya.module_context():
-        for module_info in pkgutil.iter_modules(["modules"]):
-            if module_info.name.startswith("_"):
-                continue
-            saya.require(f"modules.{module_info.name}")
+    doReloadModules()
     # 启动！
     app.launch_blocking()
